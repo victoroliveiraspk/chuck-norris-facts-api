035f64d828559f4da723a02169a8e6ffdf247c3b
Victor Oliveira <victoroliveiraspk@gmail.com>
2022-06-13 00:30:42 -0300

    refactoring tests e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,8 +2,10 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { randomFactQuery } from './queries/random-fact.query';
+import { searchFactsQuery } from './queries/search-facts.query';
 
-describe('AppController (e2e)', () => {
+describe('Graphql (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
@@ -15,10 +17,45 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('random fact', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/graphql')
+      .send({ query: randomFactQuery })
       .expect(200)
-      .expect('Hello World!');
+      .then((response) => {
+        expect(response.body.data?.random).not.toBeUndefined();
+      });
+  });
+
+  it('search fact with result', () => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: searchFactsQuery, variables: { query: 'teste' } })
+      .expect(200)
+      .then((response) => {
+        expect(
+          response.body.data?.searchFacts?.result?.length,
+        ).toBeGreaterThanOrEqual(1);
+      });
+  });
+
+  it('search fact without result', () => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: searchFactsQuery, variables: { query: 'testeteste' } })
+      .expect(200)
+      .then((response) => {
+        expect(response.body?.data?.searchFacts?.result?.length).toBe(0);
+      });
+  });
+
+  it('search fact with invalid query', () => {
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: searchFactsQuery, variables: { query: 'a' } })
+      .expect(200)
+      .then((response) => {
+        expect(response.body?.errors?.length).toBeGreaterThanOrEqual(1);
+      });
   });
 });
---
+++ test/queries/search-facts.query.ts
@@ -0,0 +1,15 @@
+export const searchFactsQuery = `
+  query SearchFacts($query: String) {
+    searchFacts(query: $query) {
+      result {
+        categories
+        created_at
+        icon_url
+        id
+        updated_at
+        url
+        value
+      }
+    }
+  }
+`;
